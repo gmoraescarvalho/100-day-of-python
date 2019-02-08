00b171d9a8f32476f88ea70d66315f64b30051af
Gleydson McOak <gleydson.dev@gmail.com>
2019-02-08 15:53:22 -0200

    Delete calculo-de-descida-plane.py

--- calculo-de-descida-plane.py
+++
@@ -1,8 +0,0 @@
-# calculo apenas axemplo.
-nivelatual = input('Digite seu nivel de voo atual :')
-niveldesce = input('Digite o nivel a descer :')
-velocidade = input('Digite velocidade para descida de acordo com o manual da aeronave :')
-#formula
-# HELP IN THE LINE OF CODE 
-# resu = (((int(nivelatual) - int(niveldesce)) / 1500) / 60) * velocidade
-# ? print(resu)
